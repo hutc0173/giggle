7dc6e8d60691b776372a321d58e12f25d5ddefe9
Sammy Hutchinson <hutc0173@algonquinlive.com>
2024-02-16 10:19:44 -0500

    fixed sw path

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
 (() => {
-  navigator.serviceWorker.register('/sw.js');
+  navigator.serviceWorker.register("./sw.js");
   getData();
   addListeners();
 })();
 
 function getData() {
   //fetch some data then add it to #datalist on the page.
-  let url = 'https://random-data-api.com/api/v2/users?size=10';
+  let url = "https://random-data-api.com/api/v2/users?size=10";
   fetch(url)
     .then((resp) => {
-      if (!resp.ok) throw new Error('Failed to fetch data');
+      if (!resp.ok) throw new Error("Failed to fetch data");
       return resp.json();
     })
     .then((data) => {
@@ -20,13 +20,13 @@ function getData() {
           <p>${email}</p>
         </li>`;
         })
-        .join('');
-      document.getElementById('datalist').innerHTML = html;
+        .join("");
+      document.getElementById("datalist").innerHTML = html;
     });
 }
 
 function addListeners() {
-  document.querySelector('main h2').addEventListener('click', (ev) => {
+  document.querySelector("main h2").addEventListener("click", (ev) => {
     getData();
   });
 }
